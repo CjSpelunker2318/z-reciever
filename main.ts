2a599e8658e34ad578dec1c1d8540d564cd840eb
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-11-24 11:21:43 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function on_forever() {
-    
+basic.showString("Z-RECIEVER")
+radio.setGroup(1)
+serial.writeLine("Acceleration")
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    led.toggle(4, 4)
+    serial.writeValue("z", receivedNumber)
 })
