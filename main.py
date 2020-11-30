5d99ccb9bacc49f52c8389238f2fc1cd8f2dca52
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-11-30 10:08:05 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+
+basic.show_string("RECEIVER")
+radio.set_group(1)
+serial.write_line("Acceleration")
+
+def on_received_number(receivedNumber):
+    led.toggle(4, 4)
+    serial.write_value("z", receivedNumber)
+radio.on_received_number(on_received_number)
+
+
