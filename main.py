f1eb456ba9ef6385e0e07b7de1ad8f421ac96352
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-12-01 10:03:23 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ serial.write_line("Acceleration")
 
 def on_received_number(receivedNumber):
     led.toggle(4, 4)
+    serial.write_value("x", receivedNumber)
+    serial.write_value("y", receivedNumber)
     serial.write_value("z", receivedNumber)
 radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -3,5 +3,7 @@ radio.setGroup(56)
 serial.writeLine("Acceleration")
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     led.toggle(4, 4)
+    serial.writeValue("x", receivedNumber)
+    serial.writeValue("y", receivedNumber)
     serial.writeValue("z", receivedNumber)
 })
