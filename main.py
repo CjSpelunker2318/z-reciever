8fd11d6ff05b2bdb41421d23e3feb5a86ccd2409
Caroline Ca <69858861+CjSpelunker2318@users.noreply.github.com>
2020-12-01 10:06:33 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ serial.write_line("Acceleration")
 def on_received_number(receivedNumber):
     led.toggle(4, 4)
     serial.write_value("x", receivedNumber)
-    serial.write_value("y", receivedNumber)
-    serial.write_value("z", receivedNumber)
 radio.on_received_number(on_received_number)
+
+def on_received_number2(receivedNumber):
+    led.toggle(4, 4)
+    serial.write_value("y", receivedNumber)
+radio.on_received_number(on_received_number2)
+
+def on_received_number3(receivedNumber):
+    led.toggle(4, 4)
+    serial.write_value("z", receivedNumber)
+radio.on_received_number(on_received_number3)
+
--- main.ts
+++ main.ts
@@ -4,6 +4,12 @@ serial.writeLine("Acceleration")
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     led.toggle(4, 4)
     serial.writeValue("x", receivedNumber)
+})
+radio.onReceivedNumber(function on_received_number2(receivedNumber: number) {
+    led.toggle(4, 4)
     serial.writeValue("y", receivedNumber)
+})
+radio.onReceivedNumber(function on_received_number3(receivedNumber: number) {
+    led.toggle(4, 4)
     serial.writeValue("z", receivedNumber)
 })
